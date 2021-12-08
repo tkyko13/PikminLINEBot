3e4aad7849a580e413cfe0c0d1bc0d04cef95fb3
tkyko13 <tkyk.o13@gmail.com>
2021-12-08 14:01:56 +0900

    upd

--- api/server.js
+++ api/server.js
@@ -94,7 +94,7 @@ async function handleEvent(event) {
 app.use(express.json());
 app.post('/ifttt', async (req, res) => {
   // const totalStep = parseInt(5000 + Math.random() * 10000); // test用
-  const totalStep = req.body.totalStep;
+  const totalStep = req.query.totalStep;
   console.log('totalStep = ' + totalStep);
 
   let index = 0;
